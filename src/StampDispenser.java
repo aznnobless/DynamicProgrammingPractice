66c893597a23679b17b7a7cac451c58310ee8713
Byung Ho Lee <aznnobless@gmail.com>
2014-11-18 00:38:54 -0800

    LaserFiche WarmingUp Interview Problem Skeleton is added.

---
+++ src/StampDispenser.java
@@ -0,0 +1,48 @@
+/**
+ * Created by aznnobless on 11/18/14.
+ */
+
+/**
+ *
+ * Laserfiche Warming up Interview Problem.
+ *
+ * The StampDispenser class represents a postage stamp vending machine.
+ * The machine contains stamps of different values.
+ * The machine will always have a stamp with a value of 1 cent and the machine will never run out of any type of stamp.
+ * The machine should allow a consumer of the class to calculate the minimum number of stamps that
+ * the machine can dispense to fill a given request.
+ * Your task is to complete one of the provided implementations of the StampDispenser class: C++, C#, or Java.
+ *
+ */
+
+public class StampDispenser {
+
+    public StampDispenser() {
+
+    }
+
+    /**
+    * Constructs a new StampDispenser that will be able to dispense the given
+    * types of stamps.
+    *
+    * @param stampDenominations The values of the types of stamps that the
+    * machine should have.  Should be sorted in descending order and
+    * contain at least a 1.
+    */
+
+    public StampDispenser(int[] stampDenominations) {
+
+    }
+
+    /**
+     * Returns the minimum number of stamps that the machine can dispense to
+     * fill the given request.
+     *
+     * @param request The total value of the stamps to be dispensed.
+     */
+    public int calcMinNumStampsToFillRequest(int request)
+    {
+        return 0;
+    }
+
+}
